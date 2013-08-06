8a21d9de955b83f707418032a489d5c14ac66122
Gideon Brett <gideonb234@gmail.com>
2013-08-06 16:48:00 +0100

    Started working on Streamer

---
+++ ModeManager/Twitter/Streamer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModeManager.Twitter
+{
+    public class Streamer
+    {
+        private String _cityA;
+        private String _cityB;
+
+        public String CityA
+        {
+            get { return _cityA; }
+        }
+
+        public String CityB
+        {
+            get { return _cityB; }
+        }
+
+        /// <summary>
+        /// Create new instance of the twitter streaming wrapper. 
+        /// </summary>
+        /// <param name="cityA">Name of City A</param>
+        /// <param name="cityB">Name of City B</param>
+        public Streamer(String cityA, String cityB)
+        {
+            _cityA = cityA;
+            _cityB = cityB;
+        }
+    }
+}
